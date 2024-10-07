174e8a115f8bf70253a4f041b0768b0292fc1369
Deepak Wadhwani <66224613+imdeepakw@users.noreply.github.com.>
2024-10-07 09:58:48 -0500

    Added intial state of the game for the dealer when the user clicks the play game button.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let deckId = ""
-let ul = document.querySelector('.playersCards')
 document.querySelector('.playGame').addEventListener('click', playGame)
 
 //Getting the deck ID and saving it to local storage on pageload.
@@ -17,28 +16,43 @@ fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
 
 function playGame(){
     playerDeck()
+    dealerDeck()
 }
 
 //playerDeckInitial
+//When user hits playGame, two cards are pulled from the deck of cards api and shown to the user dynamically.
 function playerDeck(){
     fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {
         console.log(data.cards[0].images.png)
+        let ulPlayer = document.querySelector('.playersCards')
         for(let i = 0; i < data.cards.length; i++){
         let li = document.createElement('li')
         let cardImg = document.createElement('img')
-        ul.appendChild(li)
+        ulPlayer.appendChild(li)
         li.appendChild(cardImg)
         cardImg.src = data.cards[i].images.png
         }
-        
     })
 }
-// //dealer deck Initial
-// function dealerDeck(){
-//     let backOfCard = fetch('https://www.deckofcardsapi.com/static/img/back.png')
-// }
+
+function dealerDeck(){
+    fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+    .then(res => res.json())
+    .then(data => {
+        document.querySelector('.backOfCard').src = "https://www.deckofcardsapi.com/static/img/back.png"
+        console.log(data.cards[0].images.png)
+        let ulDealer = document.querySelector('.dealersCard')
+        let li = document.createElement('li')
+        let cardImg = document.createElement('img')
+        ulDealer.appendChild(li)
+        li.appendChild(cardImg)
+        cardImg.src = data.cards[0].images.png
+        
+    })
+
+}
 
 
 
