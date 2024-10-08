953767c9161b548cd2accfdf657eabe6e02790a2
Deepak Wadhwani <66224613+imdeepakw@users.noreply.github.com.>
2024-10-08 11:15:47 -0500

    Added getting an extra card functionality when the user clicks the hit button.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let deckId = ""
 document.querySelector('.playGame').addEventListener('click', playGame)
+document.querySelector('.hitCard').addEventListener('click', getCard)
 
+
+let deckId = ""
 //Getting the deck ID and saving it to local storage on pageload.
 fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
 .then(res => res.json())
@@ -25,18 +27,26 @@ function playerDeck(){
     fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {
-        console.log(data.cards[0].images.png)
+        let total = 0 
+        console.log(data.cards[0].value)
         let ulPlayer = document.querySelector('.playersCards')
         for(let i = 0; i < data.cards.length; i++){
-        let li = document.createElement('li')
-        let cardImg = document.createElement('img')
-        ulPlayer.appendChild(li)
-        li.appendChild(cardImg)
-        cardImg.src = data.cards[i].images.png
+            let li = document.createElement('li')
+            let cardImg = document.createElement('img')
+            ulPlayer.appendChild(li)
+            li.appendChild(cardImg)
+            cardImg.src = data.cards[i].images.png
+            if(data.cards[i].value === "JACK" || data.cards[i].value === "QUEEN" || data.cards[i].value === "KING" || data.cards[i].value === "ACE"){
+                console.log(data.cards[i].value)
+                total += 10
+            }else{
+                total += Number(data.cards[i].value)
+            }
         }
+        document.querySelector('.playerTotal').innerText = total
     })
 }
-
+//When the user clicks play game, this function runs and the dealer draws one card faced down and another face up. 
 function dealerDeck(){
     fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
     .then(res => res.json())
@@ -48,16 +58,25 @@ function dealerDeck(){
         let cardImg = document.createElement('img')
         ulDealer.appendChild(li)
         li.appendChild(cardImg)
-        cardImg.src = data.cards[0].images.png
-        
+        cardImg.src = data.cards[0].images.png        
     })
-
 }
 
+function getCard(){
+    //This function should fetch one card and add it to the UL deck of cards, also add the value of the card to the previous total
 
-
-// fetch(url)
-// .then(res => res.json())
-// .then(data => {
-//     console.log(data)
-// })
+    fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        let span = document.querySelector('.playerTotal')
+        let total = Number(span.textContent)
+        console.log(total)
+        let ulPlayer = document.querySelector('.playersCards')
+        let li = document.createElement('li')
+        let newCard = document.createElement('img')
+        newCard.src = data.cards[0].images.png
+        ulPlayer.appendChild(li)
+        li.appendChild(newCard)
+    })
+}
